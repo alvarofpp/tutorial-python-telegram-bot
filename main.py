88e07dfa10e3c4084ca88462a57091f2b7cc2fa9
Álvaro Paiva <alvaro.paiva@meliuz.com.br>
2022-01-07 19:29:49 -0300

    chore: Apply changes indicated by linter

--- main.py
+++ main.py
@@ -1,42 +1,51 @@
 import logging
 
 from decouple import config
-from telegram import Update
+from telegram import BotCommand, Update
 from telegram.ext import CallbackContext, CommandHandler, Filters, MessageHandler, Updater
 
 # Enable logging
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO,
 )
 
 logger = logging.getLogger(__name__)
 
+
 # Bot
-updater = Updater(token=config('API_TOKEN'), use_context=True)
-dispatcher = updater.dispatcher
+def main() -> None:
+    """Start the bot."""
+    updater = Updater(token=config('API_TOKEN'), use_context=True)
+    dispatcher = updater.dispatcher
+
+    def start(update: Update, context: CallbackContext):
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="I'm a bot, please talk to me!",
+        )
+
+    def echo(update: Update, context: CallbackContext):
+        context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+    def caps(update: Update, context: CallbackContext):
+        text_caps = ' '.join(context.args).upper()
+        context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
+
+    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+    dispatcher.add_handler(echo_handler)
+
+    caps_handler = CommandHandler('caps', caps)
+    dispatcher.add_handler(caps_handler)
+
+    BotCommand('test', 'Test description')
+
+    updater.start_polling()
+    updater.idle()
 
 
-def start(update: Update, context: CallbackContext):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
-
-
-def echo(update: Update, context: CallbackContext):
-    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
-
-
-def caps(update: Update, context: CallbackContext):
-    text_caps = ' '.join(context.args).upper()
-    context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
-
-
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
-
-echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-dispatcher.add_handler(echo_handler)
-
-caps_handler = CommandHandler('caps', caps)
-dispatcher.add_handler(caps_handler)
-
-updater.start_polling()
-updater.idle()
+if __name__ == '__main__':
+    main()
